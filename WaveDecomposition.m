452f9c1774a0a8a55e4bcffc7989808a6ab26a27
JeffreyEarly <jeffrey@jeffreyearly.com>
2018-05-07 14:55:11 -0700

    Initial commit

---
+++ WaveDecomposition.m
@@ -0,0 +1,19 @@
+model_output_folder = '/Users/jearly/Documents/ProjectRepositories/single-wave-exponential-stratification/WintersModelRuns/output_180506';
+
+WM = WintersModel(model_output_folder);
+
+[x,y,z,rho_bar,rho_prime] = WM.VariableFieldsFrom2DOutputFileAtIndex(1,'x','y','z','s1_bar','s1');
+
+[rhoFunc, ~, zIn] = InternalModes.StratificationProfileWithName('exponential');
+rhoFunction = @(z) rhoFunc(z) - rhoFunc(max(zIn)) + double(max(rho_bar));
+
+Nx = length(x);
+Ny = 1;
+Nz = length(z);
+dx = (max(x)-min(x))/(Nx-1);
+Lx = double(dx*Nx);
+Lz = max(zIn)-min(zIn);
+latitude = 0;
+
+z = linspace(min(zIn),max(zIn),Nz);
+wavemodel = InternalWaveModelArbitraryStratification([Lx, Lx, Lz], [Nx, Ny, Nz], rhoFunction, z, Nz, latitude);
