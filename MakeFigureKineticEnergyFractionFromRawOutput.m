102eae4ee825a3551831a5dd0f204d5a01a60c3d
JeffreyEarly <jeffrey@jeffreyearly.com>
2018-10-30 15:05:19 -0700

    Update MakeFigureKineticEnergyFractionFromRawOutput.m

--- MakeFigureKineticEnergyFractionFromRawOutput.m
+++ MakeFigureKineticEnergyFractionFromRawOutput.m
@@ -35,8 +35,7 @@ elseif WM.NumberOf2DOutputFiles > 1
     [rhoFunc, ~, zIn] = InternalModes.StratificationProfileWithName('exponential');
     rhoFunction = @(z) rhoFunc(z) - rhoFunc(max(zIn)) + double(min(rho_bar));
     
-    ProfileDeviation = std((rho_bar-rhoFunction(z))/max(rho_bar));
-    fprintf('Our assumed profile deviates from the recorded profile by 1 part in 10^{%d}\n',round(log10(ProfileDeviation)));
+
     
     Nx = length(x);
     Ny = 1;
@@ -45,20 +44,24 @@ elseif WM.NumberOf2DOutputFiles > 1
     Lx = double(dx*Nx);
     Lz = max(zIn)-min(zIn);
     latitude = 0;
-%     wavemodel = InternalWaveModelArbitraryStratification([Lx, Lx, Lz], [Nx, Ny, Nz], rhoFunction, z, Nz, latitude,'nEVP',2*Nz+16);
+%     wavemodel = InternalWaveModelExponentialStratification([Lx, Lx, Lz], [Nx, Ny, Nz], [5.2e-3 1300], z, Nz, latitude);
     
-    [t,u,v,rho_prime] = WM.VariableFieldsFrom2DOutputFileAtIndex(10,'t','u','v','s1');
+    ProfileDeviation = std((rho_bar-wavemodel.internalModes.rhoFunction(z))/max(rho_bar));
+    fprintf('Our assumed profile deviates from the recorded profile by 1 part in 10^{%d}\n',round(log10(ProfileDeviation)));
+
+    [t,u,v,rho_prime] = WM.VariableFieldsFrom2DOutputFileAtIndex(100,'t','u','v','s1');
     wavemodel.InitializeWithHorizontalVelocityAndDensityPerturbationFields(t,u,v,rho_prime);
 
 else
     error('No valid files found')
 end
 
-% waveHKE_full = wavemodel.Ppm_HKE_factor .* ( abs(wavemodel.Amp_plus).^2 + abs(wavemodel.Amp_minus).^2 );
-% vortexHKE_full = wavemodel.P0_HKE_factor .* abs(wavemodel.B).^2;
+waveHKE_plus_full = wavemodel.Ppm_HKE_factor .*  abs(wavemodel.Amp_plus).^2 ;
+waveHKE_minus_full = wavemodel.Ppm_HKE_factor .* abs(wavemodel.Amp_minus).^2;
+vortexHKE_full = wavemodel.P0_HKE_factor .* abs(wavemodel.B).^2;
 
-waveHKE_full =  ( abs(wavemodel.Amp_plus).^2 + abs(wavemodel.Amp_minus).^2 );
-vortexHKE_full = abs(wavemodel.B).^2;
+% waveHKE_full =  ( abs(wavemodel.Amp_plus).^2 + abs(wavemodel.Amp_minus).^2 );
+% vortexHKE_full = abs(wavemodel.B).^2;
 
 % Now we need to figure out how to display this information...
 
@@ -79,7 +82,8 @@ RedundantCoefficients = InternalWaveModel.RedundantHermitianCoefficients(Kh);
 nK = length(k);
 nModes = length(j);
 
-waveHKE = zeros(nK,nModes);
+waveHKE_plus = zeros(nK,nModes);
+waveHKE_minus = zeros(nK,nModes);
 vortexHKE = zeros(nK,nModes);
 
 for iMode = 1:1:nModes
@@ -87,13 +91,14 @@ for iMode = 1:1:nModes
         indicesForK = find( kAxis(iK) <= squeeze(Kh(:,:,1)) & squeeze(Kh(:,:,1)) < kAxis(iK+1)  & ~squeeze(RedundantCoefficients(:,:,1)) );
         for iIndex = 1:length(indicesForK)
             [i,m] = ind2sub([size(Kh,1) size(Kh,2)],indicesForK(iIndex));
-            waveHKE(iK,iMode) = waveHKE(iK,iMode) + waveHKE_full(i,m,iMode);
+            waveHKE_plus(iK,iMode) = waveHKE_plus(iK,iMode) + waveHKE_plus_full(i,m,iMode);
+            waveHKE_minus(iK,iMode) = waveHKE_minus(iK,iMode) + waveHKE_minus_full(i,m,iMode);
             vortexHKE(iK,iMode) = vortexHKE(iK,iMode) + vortexHKE_full(i,m,iMode);
         end
     end
 end
 
-
+waveHKE = waveHKE_plus + waveHKE_minus;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
@@ -158,8 +163,10 @@ fig1.PaperUnits = 'points';
 fig1.PaperPosition = FigureSize;
 fig1.PaperSize = [FigureSize(3) FigureSize(4)];
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 p1 = subplot(1,3,1);
-pcolor( k, j, log10(waveHKE.') ), xlog, ylog, shading flat, hold on
+pcolor( k, j, log10(waveHKE_plus.') ), xlog, ylog, shading flat, hold on
 plot( k_damp, j_damp, 'LineWidth', 4, 'Color', 0*[1 1 1])
 plot( k_damp, j_damp, 'LineWidth', 2, 'Color', [1 1 1])
 
@@ -184,8 +191,10 @@ cb1.Ticks = [-10 -8 -6 -4 -2 0];
 cb1.TickLabels = {'10^{-10}', '10^{-8}', '10^{-6}', '10^{-4}', '10^{-2}', '10^{0}'};
 % ylabel(cb, 'depth integrated energy (m^3/s^2)', 'FontSize', figure_axis_label_size, 'FontName', figure_font)
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 p2 = subplot(1,3,2);
-pcolor( k, j, log10(vortexHKE.') ), xlog, ylog, shading flat, hold on
+pcolor( k, j, log10(waveHKE_minus.') ), xlog, ylog, shading flat, hold on
 plot( k_damp, j_damp, 'LineWidth', 4, 'Color', 0*[1 1 1])
 plot( k_damp, j_damp, 'LineWidth', 2, 'Color', [1 1 1])
 
@@ -213,7 +222,7 @@ cb2.TickLabels = {'10^{-10}', '10^{-9}', '10^{-8}', '10^{-7}', '10^{-6}', '10^{-
 % Energy fraction
 %
 
-HKE_fraction = waveHKE./(waveHKE+vortexHKE);
+HKE_fraction = waveHKE_plus./waveHKE;
 
 % FigureSize = [50 50 figure_width_1col+8 225*scaleFactor];
 % fig1 = figure('Units', 'points', 'Position', FigureSize);
